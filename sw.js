7deddaddee32246fe7ed6dc55002073d57067a05
Sergiu <sil3ent@yahoo.com>
2018-06-15 20:29:38 +0300

    Add comments to ServiceWorker file

--- sw.js
+++ sw.js
@@ -1,9 +1,11 @@
+// Service Worker version
 const cacheName = 'rest-rev-v2';
+// Files to add to cache
 const cacheFiles = [
     '/',
     'index.html',
     'restaurant.html',
-    '/css/styles.css',
+    'css/styles.css',
     'data/restaurants.json',
     'js/dbhelper.js',
     'js/main.js',
@@ -20,9 +22,11 @@ const cacheFiles = [
     'src/img/10.jpg'
 ];
 
+// Listen for install
 self.addEventListener('install', e => {
     console.log('[ServiceWorker] Installed');
 
+    // Open new cache & add cacheFiles array
     e.waitUntil(caches.open(cacheName)
         .then(cache => {
             console.log('[ServiceWorker] Caching cacheFiles');
@@ -31,9 +35,11 @@ self.addEventListener('install', e => {
     )
 });
 
+// Listen for activation
 self.addEventListener('activate', e => {
     console.log('[ServiceWorker] Active');
 
+    // Get cache keys & delete old cache
     e.waitUntil(caches.keys().then(cacheNames => {
         return Promise.all(cacheNames.map(thisCacheName => {
             if (thisCacheName !== cacheName) {
@@ -44,9 +50,11 @@ self.addEventListener('activate', e => {
     }))
 });
 
+// Listen for resources fetch
 self.addEventListener('fetch', e => {
     console.log('[ServiceWorker] Fetching', e.request.url);
 
+    // Check if requested resource is in cache
     e.respondWith(
         caches.match(e.request).then(response => {
             if (response) console.log('[ServiceWorker] Found in cache', e.request.url);
